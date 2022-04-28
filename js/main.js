e04da8b6d779848c18f98ecb15ad8392b816f1f0
GCaringi <giannicaringi94@gmail.com>
2022-04-28 16:06:39 +0200

    Add exercise with a little bit of style.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-let firstName = "Gianni";
-let age = 28;
-let distance = 10;
+let firstName = prompt("What's your name?");
+let age = Number(prompt("How old are you?"));
+let distance = Number(prompt("How much kilometers you have to do?"));
 let price;
+
+// age = parseInt((Math.random() * 99) + 1);
+// distance = Math.round((((Math.random()* 1000) * 1) + Number.EPSILON) * 100) / 100;
+
+document.getElementById("firstName").innerHTML = firstName;
+document.getElementById("age").innerHTML = age;
+document.getElementById("distance").innerHTML = distance;
 
 if (age >= 65){
     price = Math.round(((distance * 0.21 * 0.4) + Number.EPSILON) * 100) / 100;
@@ -11,4 +18,4 @@ if (age >= 65){
     price = Math.round(((distance * 0.21) + Number.EPSILON) * 100) / 100;
 }
 
-console.log(price);
+document.getElementById("price").innerHTML = price;
