7dc2ba3e8608b3bc3c9543eebca2c506a35af571
GCaringi <giannicaringi94@gmail.com>
2022-04-28 16:09:49 +0200

    Add exercise with radom input generator

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-let firstName = prompt("What's your name?");
-let age = Number(prompt("How old are you?"));
-let distance = Number(prompt("How much kilometers you have to do?"));
-let price;
+let firstName = "Gianni"
 
-// age = parseInt((Math.random() * 99) + 1);
-// distance = Math.round((((Math.random()* 1000) * 1) + Number.EPSILON) * 100) / 100;
+let age = parseInt((Math.random() * 99) + 1);
+let distance = Math.round((((Math.random()* 1000) + 1) + Number.EPSILON) * 100) / 100;
 
 document.getElementById("firstName").innerHTML = firstName;
 document.getElementById("age").innerHTML = age;
 document.getElementById("distance").innerHTML = distance;
+
+let price;
 
 if (age >= 65){
     price = Math.round(((distance * 0.21 * 0.4) + Number.EPSILON) * 100) / 100;
